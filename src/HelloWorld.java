52715e19c9efa74515d8a793204b6c4cef93efae
Kayla Holmes <Kayla.Holmes@cdpipelinedevshop>
2022-08-15 12:06:22 -0400

    "Hello World Test"

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("This code will run");
+
+        //System.out.println("This line is missing a semicolon")
+
+        //system.out.println("This line needs to capitalize the s");
+    }
+}
